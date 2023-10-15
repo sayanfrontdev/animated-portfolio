38fa0de641906d8242fd464d66726f6b5ed4bb0b
Sayan <sayanmanda1922001@gmail.com>
2023-10-16 02:24:21 +0530

    Initial commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import Sidebar from "../sidebar/Sidebar";
+import "./navbar.scss";
+import { motion } from "framer-motion";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      {/* Sidebar */}
+      <Sidebar/>
+      <div className="wrapper">
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          Sayan#
+        </motion.span>
+        <div className="social">
+          <a href="https://www.facebook.com/profile.php?id=100038789199924&mibextid=ZbWKwL">
+            <img src="/facebook.png" alt="" />
+          </a>
+          <a href="https://www.linkedin.com/in/sayan-mandal-b07283204/">
+            <img src="/linkedin.png" alt="" />
+          </a>
+         
+          <a href="https://github.com/sayanmanda">
+            <img src="/github.png" alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
